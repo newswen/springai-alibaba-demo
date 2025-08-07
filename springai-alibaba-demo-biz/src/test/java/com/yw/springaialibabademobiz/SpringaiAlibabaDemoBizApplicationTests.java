d126609385878ddd2c938482983f9518e4957b57
yuanwen <3405453840@qq.com>
2025-08-07 20:09:22 +0800

    feat:springai-alibaba初始

---
+++ springai-alibaba-demo-biz/src/test/java/com/yw/springaialibabademobiz/SpringaiAlibabaDemoBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yw.springaialibabademobiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiAlibabaDemoBizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
